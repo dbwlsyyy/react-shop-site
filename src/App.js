f33157951d80a8897da1838a63571a096c361f6d
홍유진 <dbwlsyyy@naver.com>
2025-04-07 23:59:39 +0900

    router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { useState } from 'react';
-import './App.css';
+import './App.module.css';
 import data from './data';
 import { Navbar, Container, Row, Col, Nav } from 'react-bootstrap';
 import Card from './components/Card';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
+import Detail from './routes/Detail';
 
 function App() {
     const [shoes, setShoes] = useState(data);
+    const navigate = useNavigate();
 
     return (
         <div className="App">
@@ -13,23 +16,38 @@ function App() {
                 <Container>
                     <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Nav.Link as={Link} to="/">
+                            Home
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/detail">
+                            Detail
+                        </Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
 
-            <div className="main-bg"></div>
-            <div>
-                <Container>
-                    <Row md={3}>
-                        {shoes.map((a, i) => (
-                            <Card shoes={a} key={i} i={i} />
-                        ))}
-                    </Row>
-                </Container>
-            </div>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <>
+                            <div className="main-bg"></div>
+                            <div>
+                                <Container>
+                                    <Row md={3}>
+                                        {shoes.map((a, i) => (
+                                            <Card shoes={a} key={i} i={i} />
+                                        ))}
+                                    </Row>
+                                </Container>
+                            </div>
+                        </>
+                    }
+                />
+                <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
+
+                <Route path="*" element={<div>404 ERROR - Invalid URL </div>} />
+            </Routes>
         </div>
     );
 }
